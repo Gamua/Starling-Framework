cb92498415d1e59b10564bc993bf0b192e424260
Daniel Sperl <daniel@gamua.com>
2013-06-19 11:18:14 +0200

    updated transporter chief to use 'iOS-deploy' instead of fruitstrap
    (Fruitstrap is no longer maintained.)

--- util/transporter_chief/transporter_chief.rb
+++ util/transporter_chief/transporter_chief.rb
@@ -14,7 +14,7 @@ require 'tmpdir'
 
 $script_path = File.expand_path(File.dirname(__FILE__))
 $temp_path = Dir.mktmpdir
-$fruitstrap_executable = "#{$script_path}/fruitstrap"
+$ios_deploy_executable = "#{$script_path}/ios-deploy"
 
 def purge_temp
   FileUtils.rm_rf($temp_path)
@@ -45,7 +45,7 @@ end
 
 # prepare the options and their parser
 options = OpenStruct.new
-options.update_fruitstrap = false
+options.update_ios_deploy = false
 options.verbose = false
 options.device_id = nil
 option_parser = OptionParser.new do |opts|
@@ -60,8 +60,8 @@ option_parser = OptionParser.new do |opts|
     options.device_id = id
   end
 
-  opts.on('-u', '--update', 'Update fruitstrap binary to latest version') do
-    options.update_fruitstrap = true
+  opts.on('-u', '--update', 'Update ios-deploy binary to latest version') do
+    options.update_ios_deploy = true
   end
 
   opts.on('-v', '--verbose', 'Redirect logs of ship\'s computer to console') do
@@ -82,25 +82,25 @@ else
   option_parser.parse!
 end
 
-# update fruitstrap if it's not installed or an update was requested
-if options.update_fruitstrap || !File.exist?($fruitstrap_executable)
+# update ios-deploy if it's not installed or an update was requested
+if options.update_ios_deploy || !File.exist?($ios_deploy_executable)
   initialize_temp
 
-  if File.exist? $fruitstrap_executable
-    log "Removing current fruitstrap"
-    FileUtils.rm_rf($fruitstrap_executable)
+  if File.exist? $ios_deploy_executable
+    log "Removing current ios-deploy"
+    FileUtils.rm_rf($ios_deploy_executable)
   end
 
   Dir.chdir($temp_path) do
-    log "Fetching latest fruitstrap"
+    log "Fetching latest ios-deploy"
     quiet = options.verbose ? "" : "-q"
-    execute("git clone #{quiet} git://github.com/ghughes/fruitstrap.git", options.verbose)
-    Dir.chdir("fruitstrap/") do
-      log "Compiling fruitstrap"
-      execute("make fruitstrap", options.verbose)
+    execute("git clone #{quiet} git://github.com/phonegap/ios-deploy.git", options.verbose)
+    Dir.chdir("ios-deploy/") do
+      log "Compiling ios-deploy"
+      execute("make ios-deploy", options.verbose)
     end
   end
-  FileUtils.mv(File.join($temp_path, "/fruitstrap/fruitstrap"), $script_path)
+  FileUtils.mv(File.join($temp_path, "/ios-deploy/ios-deploy"), $script_path)
 end
 
 # install the app if a valid path was given
@@ -121,12 +121,12 @@ if path_to_app != nil && File.exist?(path_to_app)
   end
 
   if File.extname(path_to_app) == ".app"
-    # fruitstrap the app bundle
+    # ios-deploy the app bundle
     device_id_string = options.device_id == nil ? "" : " #{options.device_id}"
     device_id_parameter = options.device_id == nil ? "" : "-i #{options.device_id}"
 
     log "Beaming app to device#{device_id_string}"
-    execute("#{$fruitstrap_executable} #{device_id_parameter} -b '#{path_to_app}'", options.verbose)
+    execute("#{$ios_deploy_executable} #{device_id_parameter} -b '#{path_to_app}'", options.verbose)
     if $? != 0
       fail "Unable to deploy app to device. Run verbose to get a more specific error message"
     end
