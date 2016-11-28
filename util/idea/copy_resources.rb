4ce58acc6d846b2be0f9b1a91f54e6ec5cd11ef2
Daniel Sperl <daniel@gamua.com>
2016-11-28 15:50:52 +0100

    updated IntelliJ IDEA resource utility
    - You can now omit the 'platform' parameter to simply copy all resources.
    - Updated the README file to make up for changes in IDEA menus.

--- util/idea/copy_resources.rb
+++ util/idea/copy_resources.rb
@@ -4,19 +4,21 @@ require "rexml/document"
 require "fileutils"
 include REXML
 
+ALL_PLATFORMS = "all-platforms"
 platforms = ["ios", "android", "air-desktop"]
 script_name = File.basename(__FILE__)
 
-if ARGV.count < 2
-  puts "Usage: #{script_name} module.iml platform"
-  puts "  (platform being one of: #{platforms.join(', ')})"
+if ARGV.count < 1
+  puts "Usage: #{script_name} module.iml [platform]"
+  puts "  - platform being one of: #{platforms.join(', ')}"
+  puts "  - omit 'platform' to copy all resources"
   exit
 end
 
 module_file = ARGV[0]
-platform = ARGV[1].downcase
+platform = ARGV.count > 1 ? ARGV[1].downcase : ALL_PLATFORMS
 
-unless (platforms.include? platform)
+unless (platform == ALL_PLATFORMS or platforms.include? platform)
   puts "Invalid platform: #{platform}"
   exit
 end
@@ -26,19 +28,26 @@ unless File.exist?(module_file)
   exit
 end
 
-puts "Copying resources for #{File.basename(module_file)} (#{platform})"
+puts "Copying resources for #{File.basename(module_file)} (#{platform}) ..."
 
 module_dir = File.dirname(module_file)
 module_doc = Document.new(File.open(module_file))
 configuration = XPath.first(module_doc, "//configuration")
 output_folder = configuration.attributes["output-folder"].gsub("$MODULE_DIR$", module_dir)
+copy_count = 0
 
-XPath.each(module_doc, "//packaging-#{platform}/files-to-package/FilePathAndPathInPackage") do |entry|
+packaging_platform =
+  if platform == ALL_PLATFORMS then "//*[starts-with(name(), 'packaging-')]"
+  else "//packaging-#{platform}"
+  end
+
+XPath.each(module_doc, "#{packaging_platform}/files-to-package/FilePathAndPathInPackage") do |entry|
   file_path = entry.attributes["file-path"].gsub("$MODULE_DIR$", module_dir)
   path_in_package = entry.attributes["path-in-package"]
   puts "  #{File.expand_path(file_path)} -> #{path_in_package}"
   FileUtils.mkdir_p path_in_package
   FileUtils.cp_r File.join(file_path, "."), File.join(output_folder, path_in_package)
+  copy_count += 1
 end
 
-puts "Done!"
+puts "Copied #{copy_count} resource-folder(s)."
