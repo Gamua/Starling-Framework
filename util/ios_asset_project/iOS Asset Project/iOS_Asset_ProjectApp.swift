14e8b1eaa4ddac488115a1bf09e3e478a4ad957a
Daniel Sperl <daniel@gamua.com>
2021-07-27 16:15:00 +0200

    added Xcode project for icon and launch screen generation

---
+++ util/ios_asset_project/iOS Asset Project/iOS_Asset_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Icon_ProjectApp.swift
+//  iOS Icon Project
+//
+//  Created by Daniel Sperl on 23.07.21.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Asset_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
